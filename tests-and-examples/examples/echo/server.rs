c4cfc21267dda93d7a56e7ed62ee250c7b50b0fe
Kaige Ye <yekaige2010@gmail.com>
2020-12-08 12:31:27 +0800

    batch enable

--- tests-and-examples/examples/echo/server.rs
+++ tests-and-examples/examples/echo/server.rs
@@ -21,6 +21,12 @@ use grpcio_proto::example::echo_grpc::{create_echo, Echo};
 #[derive(Clone)]
 struct EchoService;
 
+fn gen_resp(req : &EchoRequest) -> EchoResponse {
+    let mut resp1 = EchoResponse::default();
+    resp1.set_message(req.get_message().to_string());
+    resp1
+}
+
 impl Echo for EchoService {
     fn unary_echo(&mut self, ctx: RpcContext<'_>, req: EchoRequest, sink: UnarySink<EchoResponse>) {
         let msg = format!("Hello {}", req.get_message());
@@ -72,15 +78,45 @@ impl Echo for EchoService {
     }
 
     fn bidirectional_streaming_echo(&mut self, ctx: RpcContext<'_>, mut reqs: RequestStream<EchoRequest>, mut sink: DuplexSink<EchoResponse>) {
+        const GRPC_BATCH_SIZE : usize = 100;
         let f = async move {
+            // sink.enhance_batch(true);
+            // sink.send_all(
+            //     &mut reqs.map(|req| req.map(|req| (gen_resp(&req), WriteFlags::default()))),
+            // )
+            //     .await?;
+            // sink.close().await?;
+            // Ok(())
+
+            sink.enhance_batch(true);
+            // let mut v : Vec<EchoResponse> = Vec::with_capacity(GRPC_BATCH_SIZE);
+            let mut i : usize = 0;
+            let mut v : Vec<EchoResponse> = Vec::with_capacity(GRPC_BATCH_SIZE);
             while let Some(req) = reqs.try_next().await? {
                 // let msg = format!("Hello1 {}", req.get_message());
                 // println!("server got: {:?}", msg);
-                let mut resp1 = EchoResponse::default();
-                // resp1.set_message("server got: ".to_string() + &*msg);
-                resp1.set_message(req.get_message().to_string());
-                sink.send((resp1, WriteFlags::default())).await?;
+                // let mut resp1 = EchoResponse::default();
+                // // resp1.set_message("server got: ".to_string() + &*msg);
+                // resp1.set_message(req.get_message().to_string());
+                // sink.send((resp1, WriteFlags::default())).await?;
+                v.push(gen_resp(&req));
+                i += 1;
+                if i % GRPC_BATCH_SIZE == 0 {
+                    let v2 = v;
+                    v = Vec::with_capacity(GRPC_BATCH_SIZE);
+                    let mut  stream = stream::iter(v2.into_iter().map(move |r| {
+                        (r, WriteFlags::default())
+                        // Ok
+                    }).map(Ok));
+                    // println!("i = {}", i);
+                    sink.send_all(&mut stream).await?;
+                }
             }
+            let mut  stream = stream::iter(v.into_iter().map(move |r| {
+                (r, WriteFlags::default())
+                // Ok
+            }).map(Ok));
+            sink.send_all(&mut stream).await?;
             // let mut resp1 = EchoResponse::default();
             // resp1.set_message("server done".to_string());
             // sink.send((resp1, WriteFlags::default())).await?;
